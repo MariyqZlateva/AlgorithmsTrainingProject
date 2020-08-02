271d43e2486c406cad670191e40b900276f01c3d
MariyaZlateva <mimitoooo@abv.bg>
2020-08-02 22:19:59 +0300

    Added SelectionSort package and class.

--- src/com/mariazlateva/basic/buublesort/BubbleSortTrainingClass.java
+++ src/com/mariazlateva/basic/buublesort/BubbleSortTrainingClass.java
@@ -5,10 +5,20 @@ import com.mariazlateva.basic.Sorter;
 class BubbleSortTrainingClass implements Sorter {
     @Override
     public void sort(int[] array) {
+        for (int lastElem = array.length - 1; lastElem > 0; lastElem--) {
+            for (int i = 0; i < lastElem; i++) {
+                this.swap(array, i, i + 1);
+            }
+        }
+    }//end of sort method
 
-    }//end of method sort
+    private void swap(int[] array, int elemOne, int elemTwo) {
+        if (elemOne == elemTwo) {
+            return;
+        }
+        int temp = array[elemOne];
+        array[elemOne] = array[elemTwo];
+        array[elemTwo] = temp;
+    }//end of swap method
 
-    private void swap(int[] array, int elementOne, int elementTwo){
-
-    }
 }//end of class BubbleSortTrainingClass
---
+++ src/com/mariazlateva/basic/selectionsort/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.mariazlateva.basic.selectionsort;
+
+import com.mariazlateva.basic.Sorter;
+
+public class SelectionSort implements Sorter {
+    // == public methods ==
+    @Override
+    public void sort(int[] array) {
+        for (int last = array.length - 1; last > 0; last--) {
+            int indexHoldingMaxValue = last;
+            for (int i = 0; i < last; i++) {
+                if (array[i] > array[indexHoldingMaxValue]) {
+                    indexHoldingMaxValue = i;
+                }
+            }
+            this.swap(array, last, indexHoldingMaxValue);
+        }
+    }//end of method sort
+
+    // == private methods ==
+    private void swap(int[] array, int elementOne, int elementTwo) {
+        int temp = array[elementOne];
+        array[elementOne] = array[elementTwo];
+        array[elementTwo] = temp;
+    }
+}//end of class SelectionSort
